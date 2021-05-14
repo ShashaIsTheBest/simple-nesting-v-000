f15d1978d2f983130791ba40a50b3229f235395a
Shasha Yan <shashayan@gmail.com>
2021-05-14 19:01:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -80,12 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-
+programmer_hash
 end
 
 def changing_alan
+	alans_new_info = "GUI"
+	programmer_hash[:alan_kay][:known_for] = "GUI"
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -102,12 +103,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = "GUI"
-
+		programmer_hash
 end
 
 def adding_to_dennis
+	programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -123,5 +123,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+	programmer_hash
 end
