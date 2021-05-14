97525dcc8b54be8d1c4fc04426236138339ccc58
Shasha Yan <shashayan@gmail.com>
2021-05-14 19:16:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchies][:languages].first
+	programmer_hash[:dennis_ritchies][:languages]
 end
 
 def adding_matz
@@ -80,7 +80,7 @@ def adding_matz
         }
      }
 	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-	programer_hash
+	programmer_hash
  end
 
 def changing_alan
