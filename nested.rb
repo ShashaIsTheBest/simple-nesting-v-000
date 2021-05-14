dbcc9cd781f109d574df5ce5f0a0eb301844e717
Shasha Yan <shashayan@gmail.com>
2021-05-14 19:11:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
         }
      }
 	 end
-	programmer_hash[:yukihiro_matsumoto][:known_for] => "Ruby"
-	programmer_hash[:yukihiro_matsumoto][:languages] => ["LISP", "C"]
+	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 	programer_hash
 
 def changing_alan
